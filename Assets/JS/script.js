62955a684c2914afc6cf1b92e88608c7b0752f42
Casey <caseyderiso@gmail.com>
2020-10-08 19:33:41 -0500

    set up user input and generate password function

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,35 @@
 // Assignment code here
 
+// get input from user
+// ask user for password length 8-128 characters
+var passwordLength = window.prompt(
+  "Please choose a password length between 8 and 128 characters"
+);
+// validate input
+if (passwordLength <= 7 || passwordLength >= 129 || passwordLength === null) {
+  window.alert(
+    "Invalid input, please choose a number between 8-128 characters"
+  );
+  // send back to window prompt for valid input
+  /// some validation code
+}
+
+// ask user for lower case characters
+var passwordLowerCase = window.confirm(
+  "Would you like to include lower case characters?"
+);
+// ask user for upper case characters
+var passwordUpperCase = window.confirm(
+  "Would you like to include upper case characters?"
+);
+// ask user for special characters
+var passwordSpecialCharacters = window.confirm(
+  "Would you like to include special characters?"
+);
+
+var generatePassword = function (passwordLength, passwordLowerCase, passwordUpperCase, passwordSpecialCharacters) {
+  console.log(passwordLength, passwordLowerCase, passwordUpperCase, passwordSpecialCharacters)
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
