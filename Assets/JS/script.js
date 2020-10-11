241909e1b9963343a648297a2da9b5b3f0902958
Casey <caseyderiso@gmail.com>
2020-10-11 15:09:37 -0500

    restructured prompts to display after the generate button has been pressed.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,41 +1,12 @@
 // Initialize app
-
-// create validation function to only recieve password length 8-128. 
-// default to 10 characters
-let passwordLength = 10;
-const askLength = function() {
-  passwordLength = window.prompt(
-  "Please choose a password length between 8 and 128 characters"
-  );
-  // validate input
-  if (passwordLength <= 7 || passwordLength >= 129 || passwordLength === null || isNaN(parseFloat(passwordLength)) ) {
-    window.alert(
-    "Invalid input, please choose a number between 8-128 characters"
-    );
-    // send back to window prompt for valid input
-    askLength();
-  }
-  return passwordLength;
+// default output object to 10 characters, all false
+let output = {
+  length: 10,
+  lowerCase: false,
+  upperCase: false,
+  numbers: false,
+  specialCharacters: false
 };
-// ask user for password length 8-128 characters
-askLength();
-
-// ask user for lower case characters
-let passwordLowerCase = window.confirm(
-  "Would you like to include lower case characters?"
-);
-// ask user for upper case characters
-let passwordUpperCase = window.confirm(
-  "Would you like to include upper case characters?"
-);
-// ask user for special characters
-let passwordSpecialCharacters = window.confirm(
-  "Would you like to include special characters?"
-);
-// ask user for numeric characters
-let passwordNumbers = window.confirm(
-  "Would you like to include numeric characters?"
-);
 
 // create array for character codes
 const arrayFromLowToHigh = function (low, high) {
@@ -58,27 +29,57 @@ const SpecialCharCodes = arrayFromLowToHigh(33, 47)
   .concat(arrayFromLowToHigh(91, 96))
   .concat(arrayFromLowToHigh(123, 126));
 
-// generate the password
-const generatePassword = function (
-  passwordLength,
-  passwordLowerCase,
-  passwordUpperCase,
-  passwordSpecialCharacters,
-  passwordNumbers,
-) {
+// create validation function to only recieve password length 8-128. 
+const askLength = function() {
+  passwordLength = window.prompt(
+  "Please choose a password length between 8 and 128 characters"
+  );
+  // validate input
+  if (passwordLength <= 7 || passwordLength >= 129 || passwordLength === null || isNaN(parseFloat(passwordLength)) ) {
+    window.alert(
+    "Invalid input, please choose a number between 8-128 characters"
+    );
+    // send back to window prompt for valid input
+    askLength();
+  }
+  output.length = passwordLength;
+};
+
+// ask user which types of characters they would like to add to password. 
+const askChars = function() {
+  // ask user for lower case characters
+  output.lowerCase = window.confirm(
+    "Would you like to include lower case characters?"
+  );
+  // ask user for upper case characters
+  output.upperCase = window.confirm(
+    "Would you like to include upper case characters?"
+  );
+  // ask user for special characters
+  output.specialCharacters = window.confirm(
+    "Would you like to include special characters?"
+  );
+  // ask user for numeric characters
+  output.numbers = window.confirm(
+    "Would you like to include numeric characters?"
+  );
+};
+
+// generate the password, pass output onject into function. 
+const generatePassword = function (obj) {
   // create an array which will contain all possible character codes
   let charCodes = [];
   // default charCodes to be lower case if user chooses all false parameters
-  if (!passwordLowerCase && !passwordUpperCase && !passwordSpecialCharacters && !passwordNumbers)
+  if (!output.lowerCase && !output.upperCase && !output.specialCharacters && !output.numbers)
     charCodes = LowerCaseCharCodes;
   // add lower case char codes if user selects them
-  if (passwordLowerCase) charCodes = charCodes.concat(LowerCaseCharCodes);
+  if (output.lowerCase) charCodes = charCodes.concat(LowerCaseCharCodes);
   // add upper case char codes if user selects them
-  if (passwordUpperCase) charCodes = charCodes.concat(UpperCaseCharCodes);
+  if (output.upperCase) charCodes = charCodes.concat(UpperCaseCharCodes);
   // add special char codes if user selctes them
-  if (passwordSpecialCharacters) charCodes = charCodes.concat(SpecialCharCodes);
+  if (output.specialCharacters) charCodes = charCodes.concat(SpecialCharCodes);
   // add numeric char codes if user selects them
-  if (passwordNumbers) charCodes = charCodes.concat(NumberCharCodes);
+  if (output.numbers) charCodes = charCodes.concat(NumberCharCodes);
 
   // create an array which will contain our password characters
   let passwordCharacters = [];
@@ -97,13 +98,19 @@ const generatePassword = function (
 const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-(function writePassword() {
-  let password = generatePassword(passwordLength, passwordLowerCase, passwordUpperCase, passwordSpecialCharacters, passwordNumbers);
+function writePassword() {
+  // ask for password length
+  askLength();
+  // ask for character types. 
+  askChars();
+  // generate the password
+  let password = generatePassword(output);
+  // find the text input box for the generated password
   const passwordText = document.querySelector("#password");
-
+  // change value of text box to generated password. 
   passwordText.value = password;
+};
+
 
   // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword);
-})();
-
